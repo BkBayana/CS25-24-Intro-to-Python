f5e91d8b1f07d97e8bb62bce901c8f4dbf802281
bayana <122169908+BkBayana@users.noreply.github.com>
2024-09-27 11:36:43 +0600

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,85 @@
+# num = 0
+# while num < 5:
+#     print(num)
+#     num += 1
+# else:
+#     print('and')
+
+# a= 2
+# while a < 6:
+#     print(a)
+#     a += 1
+# else:
+#     print('buy')
+
+# num1 = 0
+# while num1 < 2:
+#     print("number one" )
+#     num2 = 1
+#     while num2 < 3:
+#         print('second number', )
+#         num2 += 1
+#     num1 += 1
+# else:
+#     print('end of the programm')
+
+# таблица умножения
+# num1 = 1
+# while num1 < 6:
+#     num2 =1
+#     while num2 < 6:
+#         print(f'{num1}*{num2}')
+#
+#         num2 += 1
+#     num1 += 1
+num1 = 1
+while num1 < 2:
+    num2 = 1
+    while num2 < 2:
+        print(f'{num1} * {num2}')
+        num2 += 1
+
+    num2 = 3
+    while num2 < 4:
+        print(f'{num1} * {num2}')
+        num2 += 1
+
+    num2 = 6
+    while num2 < 7:
+        print(f'{num1} * {num2}')
+        num2 += 1
+    num1 = 3
+    num2 = 1
+    while num2 < 2:
+        print(f'{num1} * {num2}')
+        num2 += 1
+
+    num2 = 3
+    while num2 < 4:
+        print(f'{num1} * {num2}')
+        num2 += 1
+
+    num2 = 9
+    while num2 < 10:
+        print(f'{num1} * {num2}')
+        num2 += 1
+    num1 = 9
+    num2 = 1
+    while num2 < 2:
+        print(f'{num1} * {num2}')
+        num2 += 1
+
+    num2 = 3
+    while num2 < 4:
+        print(f'{num1} * {num2}')
+        num2 += 1
+
+    num2 = 9
+    while num2 < 10:
+        print(f'{num1} * {num2}')
+        num2 += 1
+
+    num1 += 1
+
+
+
