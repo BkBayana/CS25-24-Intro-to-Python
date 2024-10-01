1e7ad83617df29d3d1635c1d9df52f0806330101
bayana <122169908+BkBayana@users.noreply.github.com>
2024-10-01 22:19:21 +0600

    Add files via upload

---
+++ hW.py
@@ -0,0 +1,36 @@
+# таблица умножения
+# for i in range(1, 2):
+#     for f in range(2, 9, 2):
+#         if f == 2 or f == 4 or f == 8:
+#             print(f'{i} * {f} = {i * f}')
+
+#
+# while cont break
+# a = 13
+# while a < 15:
+#     a += 1
+#     if a == 13:
+#         break
+#     print(" Число равно 13")
+
+# age = 1
+# while age < 3:
+#     age += 1
+#     if age == 3:
+#         continue
+#     print('you are child')
+
+# for num in range(1, 3):
+#     if num % 3 == 0:
+#         print(f"Нашли число, кратное 3")
+#         break
+#
+# for num in range(1, 5):
+#     if num % 2 == 0 and num % 3 == 0:
+#         continue
+#     print(num)
+
+# for num in range(1, 5):
+#     if num > 2 == 0 or num % 3 == 0:
+#        break
+#     print('right')
